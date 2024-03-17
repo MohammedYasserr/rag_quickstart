f146de9c3b72dd53813f55b38ed40e73d6f03288
MohammedYasserr <bmbmyasser@gmail.com>
2024-03-17 01:37:12 +0100

    importing more modules from langChain

--- main/app.py
+++ main/app.py
@@ -1,2 +1,5 @@
 from langchain_community import output_parsers
 from langchain_core import prompts
+from pydantic import BaseModel
+from langchain_community import vectorstores
+from langchain_core import embeddings
