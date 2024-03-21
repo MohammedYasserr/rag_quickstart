4077722f811780ee9aca8a4e546cf554ced65044
MohammedYasserr <bmbmyasser@gmail.com>
2024-03-21 17:06:47 +0100

    Solved the load_dotenv installation problem

--- main/app.py
+++ main/app.py
@@ -4,10 +4,12 @@ from pydantic import BaseModel
 from langchain_community import vectorstores
 from langchain_core import embeddings
 from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
 
 
+
+# using the apikey
+API_KEY = load_dotenv(dotenv_path=('C:/Users/Yasser/Downloads/rag_quickstart/.env'))
 
 # Inializing an instance
-llm = ChatOpenAI(model='gpt-3.5-turbo-1106' , temperature=0.5)
-
-# Todo, solve the dotenv installing problem
+llm = ChatOpenAI(model='gpt-3.5-turbo-1106' , temperature=0.5 , api_key=API_KEY)
